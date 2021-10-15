24067b4a92b870d312eae066e2fe58b8ce83cb87
ShotaIasashvili <shotaiasashvili@gmail.com>
2021-10-15 10:40:18 +0400

    Initial commit

---
+++ app/src/main/java/com/example/exam5/Person.java
@@ -0,0 +1,41 @@
+package com.example.exam5;
+
+public class Person {
+
+    private String userName;
+    private String email;
+    private String phone;
+    private String fullName;
+    private String jemali;
+    private String birthday;
+    private String gender;
+
+public Person(String userName, String email, String phone, String fullName, String jemali, String birthday, String gender) {
+    this.userName=userName;
+    this.email = email;
+    this.phone = phone;
+    this.fullName = fullName;
+    this.jemali = jemali;
+    this.birthday = birthday;
+    this.gender = gender;
+
+}
+
+public String getId() { return userName; }
+
+public String getEmail() {return email;}
+
+public String phone() {return phone;}
+
+public String fullName() {return fullName;}
+
+public String jemali() {return jemali;}
+
+public String birthday() {return birthday;}
+
+public String gender() {return gender;}
+
+	)
+
+
+}
